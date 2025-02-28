d9c88ae056329ff7ba41876551caec100be319b7
Aditya <kumarpavanaditya@gmail.com>
2025-02-28 22:31:36 +0530

    done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://kumarpavanaditya:<db_password>@cluster0.bvdt6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
